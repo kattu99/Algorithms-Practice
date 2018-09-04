63b418b000eced7ec8708e0d66f4a73ad14e4054
kattu99 <kataria.rahul172@gmail.com>
2018-09-04 09:45:16 -0400

    Added

---
+++ Trie/WhatIsTrie.java
@@ -0,0 +1,6 @@
+/* Trie, also called prefix tree, is a special form of Nary tree. In this card, we will go deep into the implementation of Trie and talk about how 
+to use this data structure to solve problems. Typically, a trie is used to store strings. Each Trie node represents a string (a prefix). Each node might have
+several children nodes while the paths to different children nodes represent different characters. And the strings on the child nodes represent will be the origin
+string represented by the node itself plus the character on the path. 
+
+Root node is assocuated with an empty string. All descendants of a node have a common prefix associated with that node. */

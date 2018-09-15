b534cf5dfd95818f81e28120589d335dd2b2412b
kattu99 <kataria.rahul172@gmail.com>
2018-09-14 23:44:19 -0400

    added to leet code

---
+++ Medium/LongestSubstringWithNoDups.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character,Integer> hashMap = new HashMap<Character,Integer>();
+        int max = 0;
+        int j = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (hashMap.containsKey(s.charAt(i))) {
+                j = Math.max(j, hashMap.get(s.charAt(i)) + 1);
+            }
+            hashMap.put(s.charAt(i),i);
+            max = Math.max(max, i-j+1);
+        }
+        return max;
+    }
+}
